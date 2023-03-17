8f1560ff43f7788a693bddd493f1bbca034fd82c
Juan David Martinez <juandmartinez07@gmail.com>
2023-03-17 13:15:07 -0500

    Primera version

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+import loader from './components/loader.js'
+import showMenu from './components/showMenu.js'
+import showCart from './components/showCart.js'
+
+/* UI Elements */
+/* Ocultar loader */
+loader()
+
+/* Mostrar menu */
+showMenu()
+
+/* Mostrar carrito */
+showCart()
+
+/* End UI Elements */
+
+/* Product */
